daadd976e892bb8214c2021e4caf7b8a6ac2f856
jsilence <github@jsilence.org>
2014-06-30 18:44:17 +0000

    made the queue durable and added acknowledegement after processing to make sure data is processed more robust.

--- datauploader.py
+++ datauploader.py
@@ -14,7 +14,7 @@ import pika
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 mqchannel = connection.channel()
 # @todo: split into different MQ channels for each probe
-mqchannel.queue_declare(queue='probedata')
+mqchannel.queue_declare(queue='probedata', durable=True)
 
 # extract feed_id and api_key from environment variables
 FEED_ID = os.environ["FEED_ID"]
@@ -53,6 +53,7 @@ def mqcallback(ch, method, properties, body):
     datastreams[datapoint[0]].at = datetime.datetime.fromtimestamp(datapoint[1])
     try:
         datastreams[datapoint[0]].update()
+        ch.basic_ack(delivery_tag = method.delivery_tag)
     except requests.HTTPError as e:
         print "HTTPError({0}): {1}".format(e.errno, e.strerror)
 
@@ -63,15 +64,15 @@ def main():
   feed = api.feeds.get(FEED_ID)
 
   # setting up datastreams
-  channels = ['load_avg', 'pressure', 'temperature']
+  channels = ['load_avg', 'pressure', 'temperature', 'humidity']
   for channel in channels:
     datastreams[channel] = get_datastream(feed, channel)
     datastreams[channel].max_value = None
     datastreams[channel].min_value = None
 
-  # @todo: use delivery confirmations: 
+  # @todo: use delivery confirmations: https://github.com/jsilence/i2c-raspberrypi-xively/issues/3
   # http://pika.readthedocs.org/en/latest/examples/blocking_delivery_confirmations.html
-  mqchannel.basic_consume(mqcallback, queue='probedata', no_ack=True)
+  mqchannel.basic_consume(mqcallback, queue='probedata')
 
   try:
     mqchannel.start_consuming()
